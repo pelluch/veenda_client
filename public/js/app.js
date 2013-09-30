381b192374f770e2443820f39366ba2915cfc41e
vicentejbr <vicentejbr@gmail.com>
2013-09-30 18:24:12 -0400

    Implementacion lista de mapa de google con posiciones #1132565

--- public/js/app.js
+++ public/js/app.js
@@ -61,7 +61,12 @@ App.DispatchedOrdersDispatchedOrderController = Ember.ObjectController.extend({
 App.Marker = Ember.Object.extend({
 });
 App.MapView = Ember.View.extend({
-
+  id: 'map_canvas',
+  tagName: 'div',
+  attributeBindings: ['style'],
+  style:"width:85%; height:400px",
+  map:null,
+  markers:[],
   didInsertElement: function(event) {
     var dispatcher_latitude = this.get('context').get('dispatcher_latitude');
     var dispatcher_longitude = this.get('context').get('dispatcher_longitude');
@@ -72,7 +77,34 @@ App.MapView = Ember.View.extend({
     console.log(dispatcher_longitude);
     console.log(destination_latitude);
     console.log(destination_longitude);
+    var mapOptions = {
+      center: new google.maps.LatLng(dispatcher_latitude, dispatcher_longitude),
+      zoom: 10,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    console.log(this.get('context').get('dispatcher_latitude'));
+    var controller = this.get("controller");
+    var map = new google.maps.Map(this.$().get(0),mapOptions);
     
+    this.set("map",map);
+    
+    var that = this;
+    
+    var current_pos = new google.maps.LatLng(dispatcher_latitude, dispatcher_longitude);
+    var end_pos = new google.maps.LatLng(destination_latitude, destination_longitude);
+    var locationArray = [current_pos, end_pos];
+    var locationNameArray = ['Posición Actual', 'Posición Llegada'];
+    var locationColorArray = ['red', 'green'];
+
+    var coord;
+    for (coord in locationArray) {
+      new google.maps.Marker({
+        position: locationArray[coord],
+        map: map,
+        title: locationNameArray[coord],
+        icon: "http://maps.google.com/mapfiles/ms/icons/"+locationColorArray[coord]+"-dot.png"
+      });
+    }
   }
 });
 // Close Google Map
