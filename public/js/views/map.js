349f7619be6538aba75f0f2eaaf8596cd1e7fe5f
vicentejbr <vicentejbr@gmail.com>
2013-10-12 17:24:46 -0400

    cambio de distancia en mapa y simbolo e instalacion de bootstrap 3 #1146243

--- public/js/views/map.js
+++ public/js/views/map.js
@@ -36,12 +36,29 @@ App.MapView = Ember.View.extend({
     //alert(distance);
     var coord;
     for (var coord = 0; coord < 2; coord++) {
+      var image = new google.maps.MarkerImage("gps-icon.png",
+          new google.maps.Size(60, 60),
+          new google.maps.Point(0,0),
+          new google.maps.Point(19, 19)
+      );
       new google.maps.Marker({
         position: locationArray[coord],
         map: map,
         title: locationNameArray[coord],
-        icon: "http://maps.google.com/mapfiles/ms/icons/"+locationColorArray[coord]+"-dot.png"
+        icon: image
       });
+      var populationOptions = {
+        strokeColor: '#37b4f0',
+        strokeOpacity: 0.3,
+        strokeWeight: 1,
+        fillColor: '#58d2ff',
+        fillOpacity: 0.25,
+        map: map,
+        center: locationArray[coord],
+        radius: 200
+      };
+      // Add the circle for this city to the map.
+      cityCircle = new google.maps.Circle(populationOptions);
     }
     return distance;
   },
