78ebbae1f53fa8da8010e383c227b5eed80f95e4
vicentejbr <vicentejbr@gmail.com>
2013-10-17 16:12:35 -0300

    Arreglo #1151838

--- public/js/views/map.js
+++ public/js/views/map.js
@@ -2,7 +2,7 @@ App.MapView = Ember.View.extend({
   id: 'map_canvas',
   tagName: 'div',
   attributeBindings: ['style'],
-  style:"margin-left:auto; height: 350px",
+  style:"margin-left:auto; height: 320px",
   map:null,
   markers:[],
   didInsertElement: function(event) {
