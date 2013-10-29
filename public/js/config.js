9b7a676ef2283b51615c80df5ef81f29ef2963c7
vicentejbr <vicentejbr@gmail.com>
2013-10-29 00:28:48 -0300

    Mostrar distancia en lista de pedidos login #1161370

--- public/js/config.js
+++ public/js/config.js
@@ -1,3 +1,3 @@
-var VEENDA_HOST = "http://veenda-api.herokuapp.com";
+var VEENDA_HOST = "http://veenda01.herokuapp.com";
 var VEENDA_NAMESPACE = "api/v1/client";
 var VEENDA_FULL_URL = VEENDA_HOST + "/" + VEENDA_NAMESPACE;
