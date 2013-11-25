4993d3f74a4fc968aa1e517ff18b41c253d5c3e2
Pablo Lluch <pablo.lluch@gmail.com>
2013-11-25 18:30:44 -0300

    Add status to debug media query

--- cordova/platforms/android/assets/www/app/app.js
+++ cordova/platforms/android/assets/www/app/app.js
@@ -9,30 +9,38 @@ var fixHeights = function() {
 	var largestGroup = 0;
 	var groupHeight;
 	$('.btn-group').each( function() {
-		groupHeight = $(this).height();
+		groupHeight = $(this).outerHeight();
 		if(groupHeight > largestGroup) {
 			largestGroup = groupHeight;
 		}
 	});
 
 	$('.col-order-info').each( function() {
-		var btnHeight = $(this).height();
-		var diff = largestGroup - btnHeight;
-		var padding = diff/1.1;
-		if(diff > 0){
-			$(this).height(largestGroup);
-			var child = $(this).find('.btn')[0];
+		var thisHeight = $(this).outerHeight();
+		console.log('Current height: ' + thisHeight);
+		console.log('Largest: ' + largestGroup);
+		$(this).outerHeight(largestGroup);
+		
+		console.log('Current height now: ' + thisHeight);
+		// var btnHeight = $(this).height();
+		// var diff = largestGroup - btnHeight;
+		// var padding = diff/1.1;
+		// if(diff > 0){
+			
+			var child = $(this).find('.btn-height')[0];
+			var childHeight = $(child).height();
 			$(child).css({
-				"padding-top": padding
+				"top": thisHeight/2 - childHeight/2
 			});	
-		}
 
-	});
+
+		});
 }
 
+
 App.chair = new Lawnchair({
-        adapter: "dom",
-        name: "veenda_client"
-    }, function(store) {
+	adapter: "dom",
+	name: "veenda_client"
+}, function(store) {
 });
 
