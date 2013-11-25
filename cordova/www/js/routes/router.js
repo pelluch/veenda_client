784bf94b9a113c43b110be3f089690187f974db9
Pablo Lluch <pablo.lluch@gmail.com>
2013-11-25 00:25:32 -0300

    Fix a few tag mistakes, remove cordova for now until a stable version is reached

--- cordova/www/js/routes/router.js
+++
@@ -1,8 +0,0 @@
-App.Router.map(function() {
-    this.resource('orders', function() {
-      this.route('order', {path: ':order_id'});
-    });
-
-    this.route('login', {path: '/'});
-	  this.route('rating', {path: '/ratings/:order'});
-});
